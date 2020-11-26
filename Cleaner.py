d3567b0fd1c74cc9ceff20cfed1c5ac93aab9719
Phil Pappas <phil.m.pappas@gmail.com>
2020-11-27 00:27:49 +0100

    removing self from classes

--- Cleaner.py
+++ Cleaner.py
@@ -5,32 +5,32 @@ class Cleaner:
     """Cleaning functions that were used"""
 
     # used??or not?
-    def concatenate_dataframes(self, df1, df2, df3):
+    def concatenate_dataframes(df1, df2, df3):
         frames = [df1, df2, df3]
         result = pd.concat(frames)
         return result
 
-    def drop_null_values(self, df):
+    def drop_null_values(df):
         df.dropna(subset=['Title', 'Text'], inplace=True)
         return df
 
-    def drop_duplicate_values(self, df):
+    def drop_duplicate_values(df):
         df.drop_duplicates(subset=['Text'], keep='last', inplace=True)
         return df
 
-    def remove_new_line(self, df):
+    def remove_new_line(df):
         df.Text.replace('\n', ' ', regex=True, inplace=True)
         return df
 
-    def remove_multiple_spaces(self, df):
+    def remove_multiple_spaces(df):
         df.Text.replace('\s+', ' ', regex=True, inplace=True)
         return df
 
-    def remove_non_ascii_chars(self, df):
+    def remove_non_ascii_chars(df):
         df.Text.replace(r'[^\x00-\x7F]', ' ', regex=True, inplace=True)
         return df
 
-    def remove_most_noisy_words(self, df):
+    def remove_most_noisy_words(df):
         '''THE FOLLOWING REGEXES ARE EXPLAINED IN THE Find_Noisy_words NOTEBOOK'''
         df.Text.replace(r'(\{.*?\})', '', regex=True, inplace=True)
         df.Text.replace(
@@ -95,7 +95,7 @@ class Cleaner:
 
         return df
 
-    def remove_ghost_words(self, list_2d):
+    def remove_ghost_words(list_2d):
         for i in list_2d.iteritems():
             for word in i[1]:
                 if(len(word) < 1):
@@ -103,7 +103,7 @@ class Cleaner:
 
         return list_2d
 
-    def remove_single_chars(self, df):
+    def remove_single_chars(df):
         # remove chars that are single such as "s"/.
         # If you replace the apostrophe "That's" then it's left "That s
 
@@ -113,40 +113,40 @@ class Cleaner:
             r'((?<=\s)|(?<=[^a-z]\.))[a-z](?=\s|\.{1}[^a-z])', ' ', regex=True, inplace=True)
         return df
 
-    def remove_double_chars(self, df):
+    def remove_double_chars(df):
         # remove two-letters words
         df.Text.replace(r'\s+([a-z]{2})\b', ' ', regex=True, inplace=True)
         return df
 
-    def remove_links(self, df):
+    def remove_links(df):
         df.Text.replace(
             r'(https?:\/\/)?(www\.)?[a-z0-9-]+\.(com|org|net|uk)(\.[a-z]{2,3})?', ' ', regex=True, inplace=True)
         return df
 
-    def remove_dates(self, df):
+    def remove_dates(df):
         # removes 22/09/2018, 01.04.2011, 18-12-2000
         df.Text.replace(
             r'(0?[1-9]|[12][0-9]|3[01])[\/\-\.](0?[1-9]|1[012])[\/\-\.]\d{4}', ' ', regex=True, inplace=True)
         return df
 
-    def remove_years(self, df):
+    def remove_years(df):
         # TODO: IMPROVE THIS? MATCHES 2019. 1877, 1382 etc.
         df.Text.replace(r'[1-2][0-9]\d{2}', ' ', regex=True, inplace=True)
         return df
 
-    def remove_months(self, df):
+    def remove_months(df):
         df.Text.replace(
             r'(jan(uary)?|feb(ruary)?|mar(ch)?|apr(il)?|may|jun(e)?|jul(y)?|aug(ust)?|sept(ember)?|oct(ober)?|nov(ember)?|dec(ember)?)', ' ', regex=True, inplace=True)
         return df
 
-    def remove_days(self, df):
+    def remove_days(df):
         # TODO: IMPORVE THE FOLLOWING
         # removes also sat which can be a verb and sun
         df.Text.replace(
             r'((mon|tues|wed(nes)?|thur(s)?|fri|sat(ur)?|sun)(day)?)', ' ', regex=True, inplace=True)
         return df
 
-    def remove_time(self, df):
+    def remove_time(df):
         # removes 09:11 PM - doesn't remove 23:38 because there is no PM nor AM
         df.Text.replace(
             r'\b((1[0-2]|0?[1-9]):([0-5][0-9])\s+([ap]?.[m]{1}.))', ' ', regex=True, inplace=True)
@@ -155,20 +155,20 @@ class Cleaner:
             r'\b([0-1]([0-9])|([2])([0-3])):([0-5][0-9])', ' ', regex=True, inplace=True)
         return df
 
-    def remove_punctuation(self, df):
+    def remove_punctuation(df):
         # removes all punctuations but NOT: dots (.), minus (-), and percentages (5) e.g. -2.48 %
         # We might want to keep those because of the financial and world categories
         df.Text.replace(r'[^\w\d\%\.\-\s]+|[_]', ' ',
                         regex=True, inplace=True)
         return df
 
-    def remove_punctuation_v2(self, df):
+    def remove_punctuation_v2(df):
         # removes all multiple dots, hyphen (..) or surrounded by spaces
         # e.g. ".... . .", " - - - ---"
         df.Text.replace(
             r'((?<=\s)|(?<=[a-z0-9]{2}))((\.)|(\.{2,})|(\-)|(\-{2,}))(?=\s)', ' ', regex=True, inplace=True)
         return df
 
-    def remove_numbers(self, df):
+    def remove_numbers(df):
         # to do? / check remove_years() first
         return df
