fb62ad0c2cd9991c31fb16f4a1b416d633aed2a8
Phil <phil.m.pappas@gmail.com>
2021-06-13 14:50:21 +0200

    removing duplicate packages

--- packages_imported.py
+++ packages_imported.py
@@ -15,7 +15,6 @@ import plotly.express as px
 import plotly.graph_objs as go
 from plotly.offline import iplot
 from collections import Counter
-from sklearn.metrics import confusion_matrix
 import numpy as np
 import pickle
 from sklearn.pipeline import Pipeline
@@ -23,5 +22,5 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics import classification_report
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import make_scorer, recall_score, accuracy_score, precision_score, confusion_matrix
 from sklearn.model_selection import train_test_split, GridSearchCV
